87edf048c826d9e5c2dbd4f23678bacfba1627a6
Aguinaldo Santos <santosaguinaldo@hotmail.com>
2025-02-04 21:29:06 -0300

    primeiro teste automatizado

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,5 +2,11 @@
 describe('funcionalidade: login', () => {
     it ('deve fazer login com sucesso', () =>{
         cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aguinaldo.teste@gmail.com')
+        cy.get('#password').type('123456')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, aguinaldo.teste (não é aguinaldo.teste? Sair)')
+
+
     })
 })
